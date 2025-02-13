a8de638244612850d86286119858a8c8a6ac667d
shalini <s.rathore43@gmail.com>
2025-02-13 13:33:39 +0530

    uiyuitu'

---
+++ labana-react-app/backup/AddEvent.jsx
@@ -0,0 +1,151 @@
+import axios from 'axios'
+import React, { useState,useEffect,useRef } from 'react'
+import { Categories } from '../../categorydata'
+import { API_URL } from '../../Config'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import moment from 'moment'
+export default function AddEvent() {
+
+  let admin = useSelector((store1)=>store1.admin.admin)
+  let navigate = useNavigate()
+  const [eventdata,setEventdata]=useState([])
+  let dataevent = axios.get(API_URL+"event")
+  .then((d)=>setEventdata(d.data))
+  
+  const [eventsUI,seteventUI]=useState([]);
+  useEffect(()=>
+  {
+    if(!admin.islogin)
+    {
+      navigate("/adminlogin")
+    }
+    else
+    {
+      selectEvents()
+    }
+  },[dataevent])
+
+
+  let categoryUI = Categories.map((c)=>
+  {
+    return <option value={c.categoryname} id={c.cid}>{c.categoryname}</option>
+  })
+  let eventnameref = useRef()
+  let placeref  = useRef()
+  let dateref  = useRef()
+  let timeref = useRef()
+  let maxlimitref  = useRef()
+  let categoryref  = useRef()
+  let imgref = useRef()
+  
+  let selectEvents=()=>
+  {
+    let eventsUIMAP = eventdata.map((e)=>{
+      return <tr className='border'>
+         
+        <td className='border' >{e.eventname}</td>
+        <td className='border'>{e.place}</td>
+        <td className='border'>{e.date}</td>
+        <td className='border'>{e.time}</td>
+        <td className='border'>{e.maxlimit}</td>
+        <td className='border'>{e.category}</td>
+        <td className='border'><img height={100} width={100} 
+        src={API_URL+"uploads/"+e.imgpath}/></td>
+        <td className='border'><input type="button" value="Edit" onClick={()=>editFunction(e)}/></td>
+        <td className='border'><input type="button" value="Delete" onClick={()=>deleteFunction(e._id)}/></td>
+      </tr> 
+    })
+    seteventUI(eventsUIMAP)
+  }
+  let add = ()=>
+  {
+    let data ={ eventname:eventnameref.current.value,
+      place :placeref.current.value,
+      date:dateref.current.value,
+      time:timeref.current.value,
+      maxlimit:maxlimitref.current.value,    
+      category:categoryref.current.value,
+      img:imgref.current.files[0]
+    }
+    console.log(data)
+    let headers =  {headers: {'Content-Type': 'multipart/form-data'}}
+    axios.post(API_URL+"event/",data,headers)
+    .then((d)=>
+      {
+        // msg={'message':"",'eventdata':data}
+        // console.log(d.data)
+        if(d.data.message=="event added")
+        {
+          setEventdata(d.data.eventdata)
+          console.log(eventdata)
+          console.log(d.data.eventdata)
+        }
+        else
+        {
+          alert("in else of then")
+        }
+      })
+      .catch((e)=>alert(e))
+      
+  }
+  let editFunction = (eventobj)=>
+  {
+    // alert("we will edit event whose id is "+id)
+    eventnameref.current.value=eventobj.eventname
+    placeref.current.value=eventobj.place
+    dateref.current.value = moment(eventobj.date).format("DD-MM-YYYY")
+    dateref.current.defaultValue=(moment(eventobj.date).format('DD-MM-YYYY'))
+    // console.log(dateref.current.value)
+    alert(moment(eventobj.date).format('DD-MM-YYYY'))
+    // alert(dateref.current.innerText)
+    timeref.current.value=eventobj.time
+    maxlimitref.current.value=eventobj.maxlimit
+    categoryref.current.value=eventobj.category
+
+  }
+  let deleteFunction = (id)=>
+  {
+    // alert("we willdelete event whose id is "+id)
+    let x=confirm("Are you sure want to Delete ")
+    if(!x)
+    {
+      return
+    }
+    
+    axios.delete(API_URL+"event", { data:  {id:id}  })
+    .then((d)=>
+    {
+      console.log(d)
+    })
+    .catch((e)=>
+    {
+      console.log(e)
+    })
+  }
+  // 
+  return (
+    <div className='flex'>
+    <div className='w-1/2'>AddEvent 
+      <p>Event name : <input type="text" ref={eventnameref}/></p>
+      <p>place : <input type="text" ref={placeref}/> </p>
+      <p>Date : <input type="date" ref={dateref}/></p>
+      <p>time : <input type="time" ref={timeref}/></p>
+      <p>maxlimit : <input type="text" ref={maxlimitref}/></p>
+      <p>category:
+        <select  ref={categoryref}>
+          {categoryUI}
+        </select>
+
+      </p>
+      <p>Event Image : <input type="file" ref={imgref} name="" id="" /></p>
+<p>
+  <input type="button" value="Add Event " onClick={()=>add()}/>
+</p>
+</div>
+<table border="1" className='table-fixed'>
+  {eventsUI}
+</table>
+    </div>
+  )
+}
--- labana-react-app/src/components/admin/AddEvent.jsx
+++ labana-react-app/src/components/admin/AddEvent.jsx
@@ -1,150 +1,206 @@
-import axios from 'axios'
-import React, { useState,useEffect,useRef } from 'react'
-import { Categories } from '../../categorydata'
-import { API_URL } from '../../Config'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router'
-import moment from 'moment'
-export default function AddEvent() {
+import axios from 'axios';
+import React, { useState, useEffect, useRef } from 'react';
+import { Categories } from '../../categorydata';
+import { API_URL } from '../../Config';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+import moment from 'moment';
 
-  let admin = useSelector((store1)=>store1.admin.admin)
-  let navigate = useNavigate()
-  const [eventdata,setEventdata]=useState([])
-  let dataevent = axios.get(API_URL+"event")
-  .then((d)=>setEventdata(d.data))
-  
-  const [eventsUI,seteventUI]=useState([]);
-  useEffect(()=>
-  {
-    if(!admin.islogin)
+export default function AddEvent() {
+  let admin = useSelector((store1) => store1.admin.admin);
+  let navigate = useNavigate();
+  let eventnameref = useRef();
+  let placeref = useRef();
+  let dateref = useRef();
+  let timeref = useRef();
+  let maxlimitref = useRef();
+  let categoryref = useRef();
+  let imgref = useRef();
+let imgformref = useRef();
+let btnref = useRef();
+  const [eventdata, setEventdata] = useState([]);
+  // let dataevent = 
+
+  const [eventsUI, seteventUI] = useState([]);
+
+  useEffect(() => {
+    if (!admin.islogin) {
+      navigate("/adminlogin");
+    } 
+    else
     {
-      navigate("/adminlogin")
+      axios.get(API_URL + "event")
+    .then((d) => setEventdata(d.data));
+    selectEvents()
+    }
+  }, []);
+  
+  let categoryUI = Categories.map((c) => {
+    return <option value={c.categoryname} id={c.cid} key={c.cid}>{c.categoryname}</option>;
+  });
+
+  
+  let selectEvents = () => {
+    let eventsUIMAP = eventdata.map((e) => {
+      return (
+        <tr key={e._id} className="border-b">
+          <td className="p-3">{e.eventname}</td>
+          <td className="p-3">{e.place}</td>
+          <td className="p-3">{e.date}</td>
+          <td className="p-3">{e.time}</td>
+          <td className="p-3">{e.maxlimit}</td>
+          <td className="p-3">{e.category}</td>
+          <td className="p-3">
+            <img height={100} width={100} src={API_URL + "uploads/" + e.imgpath} alt={e.eventname} className="rounded" />
+          </td>
+          <td className="p-3">
+            <button onClick={() => editFunction(e)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
+              Edit
+            </button>
+          </td>
+          <td className="p-3">
+            <button onClick={() => deleteFunction(e._id)} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300">
+              Delete
+            </button>
+          </td>
+        </tr>
+      );
+    });
+    seteventUI(eventsUIMAP);
+  };
+let empty=()=>
+{
+  eventnameref.current.value =""
+   placeref.current.value=""
+   dateref.current.value=""
+   timeref.current.value=""
+   maxlimitref.current.value=""
+
+   categoryref.current.value=""
+  //  imgref.current.files[0]=""
+  imgformref.current.src=""
+}
+  let add = () => {
+    let data = {
+      eventname: eventnameref.current.value,
+      place: placeref.current.value,
+      date: dateref.current.value,
+      time: timeref.current.value,
+      maxlimit: maxlimitref.current.value,
+      category: categoryref.current.value,
+      img: imgref.current.files[0]
+    };
+    let headers = { headers: { 'Content-Type': 'multipart/form-data ' } };
+    if(btnref.current.value=="Add")
+    {
+    axios.post(API_URL + "event/", data, headers)
+      .then((d) => {
+        if (d.data.message === "event added") {
+          setEventdata(d.data.eventdata);
+          empty()
+        } else {
+          alert("in else of then");
+        }
+      })
+      .catch((e) => alert(e));
     }
     else
     {
-      selectEvents()
+      alert("we will do update here ")
+      btnref.current.value="Add"
+      empty()
     }
-  },[dataevent])
-
-
-  let categoryUI = Categories.map((c)=>
-  {
-    return <option value={c.categoryname} id={c.cid}>{c.categoryname}</option>
-  })
-  let eventnameref = useRef()
-  let placeref  = useRef()
-  let dateref  = useRef()
-  let timeref = useRef()
-  let maxlimitref  = useRef()
-  let categoryref  = useRef()
-  let imgref = useRef()
+  };
   
-  let selectEvents=()=>
-  {
-    let eventsUIMAP = eventdata.map((e)=>{
-      return <tr className='border'>
-         
-        <td className='border' >{e.eventname}</td>
-        <td className='border'>{e.place}</td>
-        <td className='border'>{e.date}</td>
-        <td className='border'>{e.time}</td>
-        <td className='border'>{e.maxlimit}</td>
-        <td className='border'>{e.category}</td>
-        <td className='border'><img height={100} width={100} 
-        src={API_URL+"uploads/"+e.imgpath}/></td>
-        <td className='border'><input type="button" value="Edit" onClick={()=>editFunction(e)}/></td>
-        <td className='border'><input type="button" value="Delete" onClick={()=>deleteFunction(e._id)}/></td>
-      </tr> 
-    })
-    seteventUI(eventsUIMAP)
-  }
-  let add = ()=>
-  {
-    let data ={ eventname:eventnameref.current.value,
-      place :placeref.current.value,
-      date:dateref.current.value,
-      time:timeref.current.value,
-      maxlimit:maxlimitref.current.value,    
-      category:categoryref.current.value,
-      img:imgref.current.files[0]
-    }
-    console.log(data)
-    let headers =  {headers: {'Content-Type': 'multipart/form-data'}}
-    axios.post(API_URL+"event/",data,headers)
-    .then((d)=>
-      {
-        // msg={'message':"",'eventdata':data}
-        // console.log(d.data)
-        if(d.data.message=="event added")
-        {
-          setEventdata(d.data.eventdata)
-          console.log(eventdata)
-          console.log(d.data.eventdata)
-        }
-        else
-        {
-          alert("in else of then")
-        }
-      })
-      .catch((e)=>alert(e))
-      
-  }
-  let editFunction = (eventobj)=>
-  {
-    // alert("we will edit event whose id is "+id)
-    eventnameref.current.value=eventobj.eventname
-    placeref.current.value=eventobj.place
-    dateref.current.value = moment(eventobj.date).format("DD-MM-YYYY")
-    dateref.current.defaultValue=(moment(eventobj.date).format('DD-MM-YYYY'))
-    // console.log(dateref.current.value)
-    alert(moment(eventobj.date).format('DD-MM-YYYY'))
-    // alert(dateref.current.innerText)
-    timeref.current.value=eventobj.time
-    maxlimitref.current.value=eventobj.maxlimit
-    categoryref.current.value=eventobj.category
 
-  }
-  let deleteFunction = (id)=>
-  {
-    // alert("we willdelete event whose id is "+id)
-    let x=confirm("Are you sure want to Delete ")
-    if(!x)
-    {
-      return
-    }
-    
-    axios.delete(API_URL+"event", { data:  {id:id}  })
-    .then((d)=>
-    {
-      console.log(d)
-    })
-    .catch((e)=>
-    {
-      console.log(e)
-    })
-  }
-  // 
+  let editFunction = (eventobj) => {
+    eventnameref.current.value = eventobj.eventname;
+    placeref.current.value = eventobj.place;
+    dateref.current.value = moment(eventobj.date).format("YYYY-MM-DD");
+    timeref.current.value = eventobj.time;
+    maxlimitref.current.value = eventobj.maxlimit;
+    categoryref.current.value = eventobj.category;
+    imgformref.current.src = API_URL+"uploads/"+eventobj.imgpath
+    btnref.current.value="update"
+    alert(btnref.current.value)
+  };
+
+  let deleteFunction = (id) => {
+    let x = confirm("Are you sure you want to delete this event?");
+    if (!x) return;
+    axios.delete(API_URL + "event", { data: { id: id } })
+      .then((d) => console.log(d))
+      .catch((e) => console.log(e));
+  };
+
   return (
-    <div>AddEvent 
-      <p>Event name : <input type="text" ref={eventnameref}/></p>
-      <p>place : <input type="text" ref={placeref}/> </p>
-      <p>Date : <input type="date" ref={dateref}/></p>
-      <p>time : <input type="time" ref={timeref}/></p>
-      <p>maxlimit : <input type="text" ref={maxlimitref}/></p>
-      <p>category:
-        <select  ref={categoryref}>
-          {categoryUI}
-        </select>
+    <div className="min-h-screen bg-gray-100 p-8">
+      <div className="max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold text-gray-800 mb-6">Add Event</h2>
+            <div className="space-y-4">
+              <div>
+                <label className="block text-gray-700">Event Name:</label>
+                <input type="text" ref={eventnameref} className="w-full p-2 border rounded" />
+              </div>
+              <div>
+                <label className="block text-gray-700">Place:</label>
+                <input type="text" ref={placeref} className="w-full p-2 border rounded" />
+              </div>
+              <div>
+                <label className="block text-gray-700">Date:</label>
+                <input type="date" ref={dateref} className="w-full p-2 border rounded" />
+              </div>
+              <div>
+                <label className="block text-gray-700">Time:</label>
+                <input type="time" ref={timeref} className="w-full p-2 border rounded" />
+              </div>
+              <div>
+                <label className="block text-gray-700">Max Limit:</label>
+                <input type="text" ref={maxlimitref} className="w-full p-2 border rounded" />
+              </div>
+              <div>
+                <label className="block text-gray-700">Category:</label>
+                <select ref={categoryref} className="w-full p-2 border rounded">
+                  {categoryUI}
+                </select>
+              </div>
+              <div>
+                <label className="block text-gray-700">Event Image:</label>
+                <input type="file" ref={imgref} className="w-full p-2 border rounded" />
+                <img ref={imgformref}></img>
+              </div>
+              <input  ref={btnref} onClick={add} type="button" value="Add"
+               className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300"/>
+                
+              
+            </div>
+          </div>
 
-      </p>
-      <p>Event Image : <input type="file" ref={imgref} name="" id="" /></p>
-<p>
-  <input type="button" value="Add Event " onClick={()=>add()}/>
-</p>
-
-<table border="1" className='table-fixed'>
-  {eventsUI}
-</table>
+          <div className="bg-white md:col-span-2 p-6 rounded-lg shadow-md overflow-x-auto">
+            <h2 className="text-2xl font-bold text-gray-800 mb-6">Event List</h2>
+            <table className="w-full">
+              <thead>
+                <tr className="bg-gray-200">
+                  <th className="p-3 text-left">Event Name</th>
+                  <th className="p-3 text-left">Place</th>
+                  <th className="p-3 text-left">Date</th>
+                  <th className="p-3 text-left">Time</th>
+                  <th className="p-3 text-left">Max Limit</th>
+                  <th className="p-3 text-left">Category</th>
+                  <th className="p-3 text-left">Image</th>
+                  <th className="p-3 text-left">Edit</th>
+                  <th className="p-3 text-left">Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {eventsUI}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
